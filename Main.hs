dcc9c4e6edf14c95a8375f68d84ff7aecba01e63
Kris Jenkins <krisajenkins@gmail.com>
2014-11-15 10:27:00 +0000

    Removing the MVar for regular recursion.
    Thanks @derkyjadex!

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Concurrent.MVar
 import qualified Data.Map as Map
 
 data Color = Green
@@ -90,18 +89,11 @@ command _ = id
 step :: (Piece -> Piece) -> Game -> Game
 step f (Game b p) = Game b (f p)
 
-runStep :: Game -> IO Game
-runStep g = do
-  putStr $ getGameStr g
-  c <- getLine
-  return (step (command c) g)
+runLoop :: Game -> IO Game
+runLoop game = do
+  putStr $ getGameStr game
+  char <- getLine
+  runLoop $ step (command char) game
 
-run :: MVar Game -> IO b
-run state = do
-  _ <- modifyMVar_ state runStep
-  run state
-
-main :: IO b
-main = do
-  state <- newMVar (Game aBoard aPiece)
-  run state
+main :: IO Game
+main = runLoop (Game aBoard aPiece)
